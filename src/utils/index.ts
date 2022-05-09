bed23208894cd383b7473b2d2a8de48700640473
chaotang <chaotang@tencent.com>
2022-05-09 15:02:31 +0800

    demo

--- src/utils/index.ts
+++
@@ -1,9 +0,0 @@
-import getInstance from './markdownIt';
-import { highlight } from './prism';
-import { getAnchorLink } from './paths';
-
-export { 
-  getInstance,
-  highlight,
-  getAnchorLink,
- };
